0eb34afa432a8c6a8c1dfc5999e2e53bb053a6ea
Tucaliuc Agnes Monalisa <43825717+monalisaven@users.noreply.github.com>
2022-05-07 14:04:37 +0300

    Update Controller.java

--- Controller/Controller.java
+++ Controller/Controller.java
@@ -1,4 +1,4 @@
-package Controller;
+package controller;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,6 +11,7 @@ import org.apache.lucene.queryparser.classic.ParseException;
 
 import com.opencsv.exceptions.CsvException;
 
+import model.CheckSpelling;
 import model.FileIndexer;
 import model.FileSearcher;
 
@@ -19,22 +20,17 @@ public class Controller {
 	private FileSearcher searcher;
 	private Document[] searchResultDocs;
 
-	
-	
 	private int lastPageIndex;
 	private int currentPageIndex;
 	
-	
-	public Controller() throws IOException, CsvException {
+	public Controller() throws IOException, CsvException 
+	{
 		indexer=new FileIndexer();
 		searcher=new FileSearcher();
-		
 	}
-	
-	
-	
-	public void search(String query) throws ParseException, IOException {
-		
+
+	public void search(String query) throws ParseException, IOException 
+	{
 		ArrayList<Document> searchResultsDocsList=searcher.search(query);
 //		System.out.println("Found "+searchResultsDocsList.size()+"Results");
 		searchResultDocs=new Document[searchResultsDocsList.size()];
@@ -43,81 +39,83 @@ public class Controller {
 		lastPageIndex=calculateResultPages()-1;
 		System.out.println("LastPage index:"+lastPageIndex);
 //		displaySearchResults();
+		if(searchResultDocs.length == 0) {
+			CheckSpelling mySpellChecker = new CheckSpelling();
+	    	mySpellChecker.checkdictionary("pony");
+		}
 	}
 	
-	
-	
-	public void setFirstSearchResultBtnsText(ArrayList<JButton> resultButtons) {
+	public void setFirstSearchResultBtnsText(ArrayList<JButton> resultButtons) 
+	{
 		currentPageIndex=0;
 		getPageResults(currentPageIndex, resultButtons);
-		
 	}
-
 	
-	private void getPageResults(int page,ArrayList<JButton> resultButtons) {
+	private void getPageResults(int page,ArrayList<JButton> resultButtons) 
+	{
 		int start=page*10;
 		int stop=calculateStopRes(start);
-		for(int i=start;i<stop;i++) {
-			JButton btn=resultButtons.get(i%10);
-
-			btn.setText(searchResultDocs[i].get("title") + " " + searchResultDocs[i].get("release_year"));
-			btn.setVisible(true);
-		}	
+		if(searchResultDocs.length > 0) {
+			for(int i=start;i<stop;i++) {
+				JButton btn=resultButtons.get(i%10);
+				btn.setText(searchResultDocs[i].get("title") + " " + searchResultDocs[i].get("release_year"));
+				btn.setVisible(true);
+			}	
+		}
 	}
-	
-	private int calculateStopRes(int start){
-
+		
+	private int calculateStopRes(int start)
+	{
 		if(currentPageIndex==lastPageIndex && searchResultDocs.length%10!=0) {
 			return start+searchResultDocs.length%10-1;
 		}
 		return start+10;
-		
 	}
-	
-	
-	public void setNextSearchResultBtnsText(ArrayList<JButton> resultButtons) {
+		
+	public void setNextSearchResultBtnsText(ArrayList<JButton> resultButtons) 
+	{
 	  currentPageIndex+=1;
 	  System.out.println("current page:"+currentPageIndex);
 	  System.out.println("last page:"+lastPageIndex);
 	  getPageResults(currentPageIndex,resultButtons);
-	  
-	
-}
-	public void setPreviousSearchResultBtnsText(ArrayList<JButton> resultButtons) {
+	}
+		
+	public void setPreviousSearchResultBtnsText(ArrayList<JButton> resultButtons) 
+	{
 		  currentPageIndex-=1;
 		  getPageResults(currentPageIndex,resultButtons);
-		  
-		
 	}
-
-	public boolean checkForNextRemainingResults() {
+	
+	public boolean checkForNextRemainingResults() 
+	{
 		return !(currentPageIndex==lastPageIndex);
 	}
 
-	public boolean checkForPreviousRemainingResults() {
+	public boolean checkForPreviousRemainingResults() 
+	{
 		return !(currentPageIndex==0);
 	}
 
-
-	private int calculateResultPages() {
+	private int calculateResultPages() 
+	{
 		if(searchResultDocs.length%10==0) {
 			return (int) Math.min(Math.ceil(searchResultDocs.length/10),10);
 		}
 		return (int) Math.min(Math.ceil(searchResultDocs.length/10)+1,10);
-		
 	}
 	
 	
-	private  void displaySearchResults() throws IOException {
+	private  void displaySearchResults() throws IOException
+	{
 		// 4. display results
         System.out.println("Found " + searchResultDocs.length + " hits.");
         for(int i=0;i<searchResultDocs.length;++i) {
             System.out.println((i + 1) + ". " + searchResultDocs[i].get("title") + "\t" + searchResultDocs[i].get("release_year"));
         }
 	}
-//	
-	public void sortByTitle(ArrayList<JButton> resultButtons) {
 
+	public void sortByTitle(ArrayList<JButton> resultButtons) 
+	{
         // create string array called names
         Document temp;
         for (int i = 0; i <  searchResultDocs.length; i++) {
@@ -131,12 +129,11 @@ public class Controller {
                 }
             }
         }
-        setFirstSearchResultBtnsText(resultButtons);
-        
+        setFirstSearchResultBtnsText(resultButtons);   
 	}
 	
-	public void sortByYear(ArrayList<JButton> resultButtons) {
-
+	public void sortByYear(ArrayList<JButton> resultButtons) 
+	{
         // create string array called names
         Document temp;
         for (int i = 0; i <  searchResultDocs.length; i++) {
@@ -150,17 +147,6 @@ public class Controller {
                 }
             }
         }
-        setFirstSearchResultBtnsText(resultButtons);
-        
-	}
-//	
-	
-	public static void main(String[] args) throws IOException, CsvException,ParseException{
-		
-		Controller controller= new Controller();
-		controller.search("Naruto");
-	}
-	
-	
-	
+        setFirstSearchResultBtnsText(resultButtons);  
+	}	
 }
